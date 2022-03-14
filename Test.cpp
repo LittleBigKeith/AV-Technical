20c7e785d5eb2bbf46d322eb2f24d61ae1bee96d
Keith Kong Hei Lok <70051933+LittleBigKeith@users.noreply.github.com>
2022-03-14 13:57:01 +0800

    AV-Technical 14/3 2pm
    Contains code of solution nmp from project Main and TesterFunction.

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+﻿#include "gtest/gtest.h"
+#include "src/gtest-internal-inl.h"
+#include "Main.h"
+#include "TesterFunction.cpp"
+
+TEST(MoneyTest, CheckMoneyInWallet) {
+	ASSERT_EQ(5000, Main::testerFunction());
+}
+
+//To run the gtest:
+//	./TesterFunction --gtest_repeat=1000 --gtest_break_on_failure
--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
 // Question 1 Install g tests and add to cmakes lists
 #include "gtest/gtest.h"
+#include <iostream>
+#include <map>
+#include <type_traits>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include "Main.h"
 
+bool GTEST_FLAG = false;
 
 const int values[] = { 1,2,3,4,5 };
 const int NVALS = sizeof values / sizeof (int);
-void function(char**);
+
+void function(char[][6]);
+void fn(const int**);
+
+class Foo;
 
 template <typename T>
 struct ptr_holder {
-  static_assert(std::is_same_v<T, std::remove_pointe<T>>);
+  static_assert(!std::is_same_v<T, std::remove_pointer<T>>);
+  T* ptr;
 };
 
-int total = 0
+struct person{
+   int age;
+   float weight;
+} person;
 
+//NB: declaration moved to Main.h
+//NB: definition moved to TesterFunction.cpp for Google Test
+/*
 class Wallet
 {
     int mMoney;
@@ -21,17 +40,13 @@ public:
     int getMoney() { return mMoney; }
     void addMoney(int money)
     {
+       std::lock_guard<std::mutex> guard(myMutex);
        for(int i = 0; i < money; ++i)
        {
           mMoney++;
        }
     }
 };
-
-struct {
-   int age;
-   float weight;
-} person;
 
 int testerFunction()
 {
@@ -46,71 +61,108 @@ int testerFunction()
    }
    return walletObject.getMoney();
 }
+*/
+//NB: declaration moved to Main.h
+//NB: definition moved to TesterFunction.cpp for Google Test
 
-
-int Main()
+int main(int argc, char *argv[])
 {
     // Question 2
     int x = 10;
     int y = 15;
-    cout << " "<< (x, y) << std::endl;
+    std::cout << "Q2: ";
+    std::cout << "(" << x << "," << y << ")" << std::endl;
 
     // Question 3 Get the function to run
-    char arr[] = { "ant", "bat", "cat", "dog", "egg", "fly" };
+    char arr[][6] = {"ant", "bat", "cat", "dog", "egg", "fly"};
+    std::cout << "Q3: ";
 	function(arr);
 
 
     // Question 4 Insert in map
-    typedef map<int, double> valmap;
+    typedef std::map<int, double> valmap;
 
     valmap m;
 
     for (int i = 0; i < NVALS; i++)
-        m.insert(make_pair(values[i], pow(values[i], .5)));
+        m.insert(std::make_pair(values[i], pow(values[i], .5)));
+    std::cout << "Q4: ";
+    valmap::iterator it = m.begin();
+    for (it; it != m.end(); ++it) {
+        std::cout << "{" << it->first << "," << it->second << "} ";
+    }     
+    std::cout << std::endl;
+    m.insert(std::pair<int, double> (6,pow(6, .5)));
+    m[1] = 2;
+    valmap::iterator it2(m.begin());    
+    for (it2; it2 != m.end(); ++it2) {
+        std::cout << "{" << it2->first << "," << it2->second << "} ";
+    }
+    std::cout << std::endl;
 
-    valmap::iterator it = 100;              
-    valmap::iterator it2(100);              
-    m.insert(1,2);
-    
     // Question 5
     int i = 1, j = 1;
-    if ( (i =! 3) && (j==1))
+    if ( (i != 3) && (j == 1))
     {
-        cout << "inside if statement\n";
+        std::cout << "Q5: ";
+        std::cout << "inside if statement\n";
     }
-
+    
     // Question 6 Store a char * pointer in template
-    ptr_holder<(void *)> p;
+    ptr_holder<char *> p;
+    std::cout << "Q6: ";
+    std::cout << sizeof(p) << "," << &p << std::endl;
+
 
     // Question 7 Fix the compiler errors and race conditions
     // Convert the testerFunction() into a google test
     int val = 0;
-    for(int k = 0; k < 1000; k++)
-    {
-        if((val = testerFunction()) != 5000)
+    std::cout << "Q7: ";
+    if (!GTEST_FLAG) {
+        for (int k = 0; k < 1000; k++)
         {
-            std::cout << "Error at count = "<<k<<" Money in Wallet = "<<val << std::endl;
-            return 1;
+            if ((val = Main::testerFunction()) != 5000)
+            {
+                std::cout << "Error at count = " << k << " Money in Wallet = " << val << std::endl;
+                return 1;
+            }
         }
+        std::cout << "passed" << std::endl;
+    }
+    else {
+        std::cout << "not tested, use gtest instead." << std::endl;
     }
 
     // Question 8
-    int n = 1;
-    int *p = &n;
-    fn(&p);
-
+    {
+        int n = 1;
+        const int* p = &n;
+        std::cout << "Q8: ";
+        fn(&p);
+        std::cout << std::endl;
+    }
+    
     // Question 9
     struct person *ptr;
-    ptr = (struct person*)malloc(sizeof(struct person));
-    ptr->age = 10;
-    ptr->weight = 55.5;
-
+    ptr = (struct person *)malloc (sizeof(struct person));
+    if (ptr) {
+        ptr->age = 10;
+        ptr->weight = 55.5;
+        std::cout << "Q9: ";
+        std::cout << ptr->age << "," << ptr->weight << std::endl;
+        free(ptr);
+    }
+    
+    
     // Question 10 Initialise foo
-    Foo foo;
+    Foo foo(int a, Wallet *b);
+    std::cout << "Q10: ";
+    std::cout << typeid(foo).name() << std::endl;
 
     // Question 11
     char c;
-    while(c = getchar() == EOF)
+    std::cout << "Q11: ";
+    while((c = getchar()) != EOF && c != '\n')
     {
         putchar(c);
     }
@@ -119,7 +171,7 @@ int Main()
 }
 
 // Do not change anything besides the ptr variable
-void function(char ptr)
+void function(char ptr[][6])
 {
 	char *ptr1;
 	ptr1 = (ptr += sizeof(int))[-2];
@@ -134,5 +186,9 @@ void fn(const int** pp)
 // Fix any compilation error
 class Foo {
 public:
-    Foo(int a, Wallet* b = NULL);
+    Foo(int a, Wallet* b);
 };
+
+Foo::Foo(int a, Wallet* b = NULL) {
+
+}
